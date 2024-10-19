57ac6facfb0f3d1e3a1b0452bfee981aa680656f
Varshini B R <167518182+Varshini-B-R@users.noreply.github.com>
2024-10-19 16:19:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
 def count_batteries_by_health(present_capacities):
+  rated_capacity = float(input("Enter the rated capacity: "))
   return {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
 
+for current_capacity in present_capacities:
+  SOH = (present_capacity / rated_capacity) * 100
+  print("SOH of " ,current_capacity, ": " ,SOH)
+
+  if 
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
