97ac9d72dcde610ba1f5e745a1043dc9f54442ff
Varshini B R <167518182+Varshini-B-R@users.noreply.github.com>
2024-10-19 16:24:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-
 def count_batteries_by_health(present_capacities):
-  rated_capacity = float(input("Enter the rated capacity: "))
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+    rated_capacity = float(input("Enter the rated capacity: "))
+    health_counts = {
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
 
-for current_capacity in present_capacities:
-  SOH = (present_capacity / rated_capacity) * 100
-  print("SOH of " ,current_capacity, ": " ,SOH)
+    for current_capacity in present_capacities:
+        SoH = (current_capacity / rated_capacity) * 100
+        print("SoH of", current_capacity, ":", SoH)
 
-  if 
+        if SoH > 80:
+            health_counts["healthy"] += 1
+        elif 62 < SoH <= 80:
+            health_counts["exchange"] += 1
+        else:
+            health_counts["failed"] += 1
+
+    print("Counts:")
+    print("Healthy:", health_counts["healthy"])
+    print("Exchange:", health_counts["exchange"])
+    print("Failed:", health_counts["failed"])
+
+
+    return health_counts
+
 
 def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+    print("Counting batteries by SoH...\n")
+    present_capacities = [113, 116, 80, 95, 92, 70]
+    counts = count_batteries_by_health(present_capacities)
+    assert(counts["healthy"] == 2)
+    assert(counts["exchange"] == 3)
+    assert(counts["failed"] == 1)
+    print("Done counting :)")
 
 
 if __name__ == '__main__':
-  test_bucketing_by_health()
+    test_bucketing_by_health()
